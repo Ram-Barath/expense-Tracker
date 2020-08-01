cdf860bcf6eabc4bacf774e179527bba0abd254d
RamBarath4 <rambarath456456@gmail.com>
2020-08-01 23:17:14 +0530

    Updates

---
+++ precache-manifest.bcb33fcacfcfe8d01abd5be8172febee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53333be4c07485dcbcde292d156c57a",
+    "url": "/expense-Tracker/index.html"
+  },
+  {
+    "revision": "c87eb2184e1c8a17551b",
+    "url": "/expense-Tracker/static/css/main.b7901255.chunk.css"
+  },
+  {
+    "revision": "a459aa6928bc471d6afc",
+    "url": "/expense-Tracker/static/js/2.1d448702.chunk.js"
+  },
+  {
+    "revision": "e46fc7d39878ee64e7b083a156855138",
+    "url": "/expense-Tracker/static/js/2.1d448702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87eb2184e1c8a17551b",
+    "url": "/expense-Tracker/static/js/main.f36b42c8.chunk.js"
+  },
+  {
+    "revision": "46003ac05674d8059648",
+    "url": "/expense-Tracker/static/js/runtime-main.dc4c2f1c.js"
+  }
+]);
