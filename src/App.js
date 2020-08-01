3d6b49a479ec528085fb956d073ca4feb07d217d
RamBarath4 <rambarath456456@gmail.com>
2020-08-01 23:12:20 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpense } from './components/IncomeExpense';
+import { TransactionList } from './components/TransactionList';
+import { TransactionForm } from './components/TransactionForm';
+import {GlobalProvider} from './context/GlobalContext';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <TransactionForm />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
